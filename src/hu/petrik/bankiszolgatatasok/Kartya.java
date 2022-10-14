54c73598395f59f5a8cf46f985d496068398bd8b
Hoós Roland <shadeeygg@gmail.com>
2022-10-14 16:41:35 +0200

    Kartya Szamla

---
+++ src/hu/petrik/bankiszolgatatasok/Kartya.java
@@ -0,0 +1,28 @@
+package hu.petrik.bankiszolgatatasok;
+
+public class Kartya extends BankiSzolgaltatas{
+
+    private Szamla szamla;
+    private String kartyaSzam;
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public Kartya(Tulajdonos tulajdonos, Szamla szamla, String kartyaSzam) {
+        super(tulajdonos);
+        this.szamla = szamla;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public boolean vasarlas(int osszeg){
+        if (szamla.aktualisEgyenleg > osszeg){
+            szamla.aktualisEgyenleg -= osszeg;
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
+}
